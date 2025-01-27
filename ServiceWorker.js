2d14df922497e77e9c28e38eb4185e8e53ee4e07
Jeff <jeffchou32@gmail.com>
2025-01-26 19:59:35 -0600

    web

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Jeff Chou-Asteroid Rush-0.1";
+const contentToCache = [
+    "Build/Asteroid Rush Github.loader.js",
+    "Build/Asteroid Rush Github.framework.js.unityweb",
+    "Build/Asteroid Rush Github.data.unityweb",
+    "Build/Asteroid Rush Github.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
